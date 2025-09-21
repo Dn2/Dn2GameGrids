96be79c821adab7eb9092af228a1ed651e3818b5
Dn2 <Dn2@users.noreply.github.com>
2025-09-21 19:28:49 -0400

    Add Hex actor class and the start of Hex features

---
+++ Source/Dn2GameGrids/Public/HexGridActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BoxGridActor.h"
+#include "HexGridActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DN2GAMEGRIDS_API AHexGridActor : public ABoxGridActor
+{
+	GENERATED_BODY()
+	
+};
