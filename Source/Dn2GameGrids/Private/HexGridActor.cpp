9e4e7e3294794f9a7b3ea00388063c7f98f088df
Dn2 <Dn2@users.noreply.github.com>
2025-09-22 16:32:51 -0400

    started on hex, swapping to dev branch

--- Source/Dn2GameGrids/Private/HexGridActor.cpp
+++ Source/Dn2GameGrids/Private/HexGridActor.cpp
@@ -3,3 +3,67 @@
 
 #include "HexGridActor.h"
 
+AHexGridActor::AHexGridActor() : Super()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	//PrimaryActorTick.bCanEverTick = false;
+
+	HexOffsetMethod = EHexOffsetMethod::HOM_RowOdd;
+}
+
+TArray<FCellInfo> AHexGridActor::CreateEmptyGrid(int32 XExtent, int32 YExtent, FGameplayTagContainer DefaultTags)
+{
+	return Super::CreateEmptyGrid(XExtent, YExtent, DefaultTags);
+}
+
+
+FVector AHexGridActor::GetCellLocationFromAddress(FCellAddress Address, bool bLocalSpace)
+{
+	FVector CellLoc = Super::GetCellLocationFromAddress(Address, bLocalSpace);
+	int32 X = Address.X;
+	int32 Y = Address.Y;
+	
+	switch (HexOffsetMethod)
+	{
+		case EHexOffsetMethod::HOM_RowOdd:
+			if (Y % 2)
+			{
+				CellLoc.Y = CellLoc.Y + (CellSize/2);
+			}
+		break;
+		
+		case EHexOffsetMethod::HOM_RowEven:
+		if (!(Y % 2))
+		{
+			CellLoc.Y = CellLoc.Y + (CellSize/2);
+		}
+		break;
+
+		case EHexOffsetMethod::HOM_ColOdd:
+		if (X % 2)
+		{
+			CellLoc.X = CellLoc.X - (CellSize/2);
+		}
+		break;
+		
+		case EHexOffsetMethod::HOM_ColEven:
+		if (!(X % 2))
+		{
+			CellLoc.X = CellLoc.X - (CellSize/2);
+		}
+		break;
+	}
+	
+	return CellLoc;
+}
+
+
+FCellAddress AHexGridActor::GetCellAddressFromLocation(FVector Location)
+{
+	return Super::GetCellAddressFromLocation(Location);
+}
+
+TArray<FVector> AHexGridActor::GetCellVertexArray(FCellAddress InAddress, bool bLocalSpace)
+{
+	return Super::GetCellVertexArray(InAddress, bLocalSpace);
+}
--- Source/Dn2GameGrids/Public/HexGridActor.h
+++ Source/Dn2GameGrids/Public/HexGridActor.h
@@ -13,5 +13,19 @@ UCLASS()
 class DN2GAMEGRIDS_API AHexGridActor : public ABoxGridActor
 {
 	GENERATED_BODY()
+
+public:	
+	// Sets default values for this actor's properties
+	AHexGridActor();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Grid|Hex")
+	EHexOffsetMethod HexOffsetMethod;
 	
+	virtual TArray<FCellInfo> CreateEmptyGrid(int32 XExtent, int32 YExtent, FGameplayTagContainer DefaultTags) override;
+
+	virtual FVector GetCellLocationFromAddress(FCellAddress Address, bool bLocalSpace=false) override;
+
+	virtual FCellAddress GetCellAddressFromLocation(FVector Location) override;
+
+	virtual TArray<FVector> GetCellVertexArray(FCellAddress InAddress, bool bLocalSpace=true) override;
 };
