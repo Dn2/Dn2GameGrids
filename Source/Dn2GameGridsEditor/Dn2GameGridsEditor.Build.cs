22fd4beec44a6b3e2de8a0fd7d4f178eade1ba31
Dn2 <Dn2@users.noreply.github.com>
2026-06-02 19:58:11 -0400

    Added Async UGridMapData asset loading

--- Source/Dn2GameGridsEditor/Dn2GameGridsEditor.Build.cs
+++ Source/Dn2GameGridsEditor/Dn2GameGridsEditor.Build.cs
@@ -4,7 +4,7 @@ public class Dn2GameGridsEditor: ModuleRules
 {
 	public Dn2GameGridsEditor(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PrivateDependencyModuleNames.AddRange(new string[] {"Core", "CoreUObject", "Engine", "Dn2GameGrids", "Slate", "SlateCore"});
+		PrivateDependencyModuleNames.AddRange(new string[] {"Core", "CoreUObject", "Engine", "Dn2GameGrids", "Slate", "SlateCore","GameplayTags"});
 		
 		PublicDependencyModuleNames.AddRange(new string[] { "UnrealEd", "AssetTools" });
 	}
